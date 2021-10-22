e3a2cbe8346e87d2bd4655090116d52d81d1dd82
FaisalTamanoJr <84402344+FaisalTamanoJr@users.noreply.github.com>
2021-10-22 17:42:20 +0300

    improved date and time formatting

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 import logging
 from logging.handlers import RotatingFileHandler
 import os
@@ -14,6 +15,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = "login"
 
